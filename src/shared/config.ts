e504b98a297aea474cdc16b16db37f07ad80c757
abdulqodir18 <abduqodirdev@gmail.com>
2025-05-19 12:12:20 +0500

    fix: update document title and refine API base URL configuration

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -3,5 +3,5 @@ interface Config {
 }
 
 export const config: Config = {
-  apiUrl: import.meta.env.VITE_BASE_URL || 'http://192.168.31.50:8080',
+  apiUrl: import.meta.env.VITE_BASE_URL,
 } as const;
